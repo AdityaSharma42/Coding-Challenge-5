266f74bea6b63b3dad8fbeae7338a393a64f9140
AdityaSharma42 <adityasharma3@usf.edu>
2024-09-21 17:16:50 -0400

    Initialize inventory with product objects

---
+++ order_management_system.js
@@ -0,0 +1,10 @@
+const inventory = [
+    { name: 'Espresso', price: 4, quantity: 20 },
+    
+    { name: 'Matcha', price: 6, quantity: 16 },
+    
+    { name: 'Cappuccino', price: 5, quantity: 18 },
+    
+    { name: 'Cheesecake', price: 7, quantity: 14 }
+    
+    ];
