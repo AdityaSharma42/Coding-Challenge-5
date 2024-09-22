94d4ada62a4ea39d8a821877e75eae1385dfafb9
AdityaSharma42 <adityasharma3@usf.edu>
2024-09-22 19:53:47 -0400

    Create placeOrder function

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Coding-Challenge-5
 Customer Order Management System
-Creating a simple employee shift management system for a small business
+
+Creating a simple system to manage customer orders for a small coffee shop
--- order_management_system.js
+++ order_management_system.js
@@ -8,3 +8,47 @@ const inventory = [
     { name: 'Cheesecake', price: 7, quantity: 14 }
     
     ];
+
+const orders= [];
+
+let newOrders={
+    customerName: "Beth", 
+    items:[
+        {productName: "Cappuccino", quantity: 1},
+        {productName: "Cheesecake", quantity: 2}
+    ],
+    status: "Completed"
+
+};
+orders.push(newOrders);
+
+console.log (orders);
+
+function placeOrder(customerName, items){
+    for (let item of items){
+        const product= inventory.find(product=> product.name===item.productName);
+        if (!product){
+            console.log (`"${item.productName}"does not exist.`);
+            return;
+        }
+        if (item.quantity>product.quantity){
+            console.log (`There is not enough ${item.productName} in stock.`);
+            return;
+        }
+        
+    }
+    for  (let item of items){
+        const product= inventory.find(product=> product.name===item.productName);
+        product.quantity -= item.quantity;
+    }
+    orders.push({
+        customerName: customerName,
+        items: items,
+        status: "Pending"
+    });
+
+    console.log (`The order has been placed for ${customerName}.`);
+}
+placeOrder("Bret", [{productName: "Matcha", quantity:3}]);
+console.log(orders);
+console.log(inventory);
